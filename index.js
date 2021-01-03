33ae8fe5923a2fd5b98248bd3e344998e8ccf965
Kyle Bloom <kylejbloom@gmail.com>
2021-01-03 16:23:55 +0000

    Bump version

--- index.js
+++ index.js
@@ -33,17 +33,17 @@ module.exports = function() {};
 module.exports = {
     get: function(pathOrData, cb) {
         var getData = (pathOrData instanceof Buffer) ?
-    		function(data, cb) { cb(null, data); } : fs.readFile;
+        function(data, cb) { cb(null, data); } : fs.readFile;
 
-    	getData(pathOrData, function(err, data) {
-    		if (err) return cb(pathOrData + ' not found.');
-    		try {
-    			var info = ttfInfo(data);
-    			cb(null, info);
-    		} catch(err) {
-    			cb(err);
-    		}
-    	});
+      getData(pathOrData, function(err, data) {
+        if (err) return cb(pathOrData + ' not found.');
+        try {
+          var info = ttfInfo(data);
+          cb(null, info);
+        } catch(err) {
+          cb(err);
+        }
+      });
     },
     getSync: function(pathOrData) {
         var data;
